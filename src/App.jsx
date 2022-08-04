9ffad8880bdda7234ad94b0188fd15ff1713b3f9
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-04 19:18:15 -0400

    basic jsx and semantic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import "./App.css";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home(props) {
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
